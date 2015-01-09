358510cf923396e2fbe947b8c83430f0326ec27b
donth77 <donth77@gmail.com>
2015-01-09 02:08:32 -0500

    Create assignment1.h

---
+++ assignment1/assignment1.h
@@ -0,0 +1,6 @@
+void doPipeCommands(Vector cmds[], int pipes);
+void pipeline(Vector* args, int argsCount);
+char* findFile(char* string, Vector* args);
+void shell(Vector args);
+int execute(Vector args, int argsCount, int pline);
+
